8ac618bad2473456b21584547220e3d883bd0883
xiaoyijie <xiaoyijie2007@126.com>
2014-12-05 09:05:36 +0800

    add StreamTheCard

---
+++ StreamTheCard/StreamTheCard/Deck.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreamTheCard
+{
+    [Serializable]
+    public class Deck
+    {
+        private List<Card> cards;
+        private Random randon = new Random();
+        public Deck()
+        {
+            cards = new List<Card>();
+            for (int suit = 0; suit <= 3; suit++)
+            {
+                for (int value = 0; value <= 13; value++)
+                {
+                    cards.Add(new Card((Card.Suits)suit, (Card.Values)value));
+                }
+            }
+        }
+        public Deck(Card[] initialCards)
+        {
+            cards = new List<Card>(initialCards);
+        }
+        public int Count { get { return cards.Count; } }
+        public void Add(Card cardToAdd)
+        {
+            cards.Add(cardToAdd);
+        }
+        public Card Deal(int index)
+        {
+            Card CardToDeal = cards[index];
+            cards.RemoveAt(index);
+            return CardToDeal;
+        }
+        public void Shuffle()
+        {
+            List<Card> NewCards = new List<Card>();
+            while (cards.Count>0)
+            {
+                int CardToMove = randon.Next(cards.Count);
+                NewCards.Add(cards[CardToMove]);
+                cards.RemoveAt(CardToMove);
+            }
+            cards = NewCards;
+        }//洗牌
+        public string[] GetCardNames()
+        {
+            string[] CardNames = new string[cards.Count];
+            for (int i = 0; i < cards.Count; i++)
+            {
+                CardNames[i] = cards[i].Name;
+            }
+            return CardNames;
+        }//显示包含的每张牌的名字
+    }
+}
---
+++ StreamTheCard/StreamTheCard/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace StreamTheCard
+{
+    [Serializable]
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        private Deck RandomDeck(int Number)
+        {
+            Deck myDeck = new Deck(new Card[]{});
+            for (int i = 0; i < Number; i++)
+			{
+                myDeck.Add(new Card((Card.Suits)random.Next(4),(Card.Values)random.Next(1,14)));
+			}
+            return myDeck;
+        }
+        private void DealCards(Deck DeckToDeal, string Title)
+        {
+            Console.WriteLine(Title);
+            while (DeckToDeal.Count>0)
+            {
+                Card nextCard = DeckToDeal.Deal(0);
+                Console.WriteLine(nextCard.Name);
+            }
+            Console.WriteLine("--------------------");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Deck deckToWrite = RandomDeck(5);
+            using (Stream output = File.Create(@"D:\Deck1.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(output, deckToWrite);
+            }
+            DealCards(deckToWrite, "What i just wrote to the file");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Deck deckFromFile;
+            using (Stream input = File.OpenRead(@"D:\Deck1.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                deckFromFile= (Deck)bf.Deserialize(input);
+            }
+            DealCards(deckFromFile, "What i read from the file");
+        }
+    }
+}
